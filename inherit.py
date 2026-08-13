51651bf53394743891ae1258d8da7f6eebda81e2
Srijan <ssrijan579@gmail.com>
2026-08-13 23:27:44 +0530

    super

--- inherit.py
+++ inherit.py
@@ -21,3 +21,33 @@ animal.speak()  # Output: Generic Animal makes a sound.
 # # # Create an instance of Dog
 dog = Dog("buddy")
 dog.speak()  # Output: Buddy barks.
+
+
+# Super Keyword
+
+# Super
+
+# Base class
+class Animal:
+    def __init__(self):
+        self.name = "Buddy"
+
+    def speak(self):
+        print(f"{self.name} makes a sound.")
+
+# # Derived class
+class Dog(Animal):
+    def __init__(self, breed):
+        super().__init__()
+        self.breed = breed
+
+    def speak(self):
+        super().speak()  # Call the base class method
+        print(f"{self.name} barks. It is a {self.breed}.")
+
+# # Create an instance of Dog
+dog = Dog("Golden Retriever")
+dog.speak()
+# Output:
+# Buddy makes a sound.
+# Buddy barks. It is a Golden Retriever.
